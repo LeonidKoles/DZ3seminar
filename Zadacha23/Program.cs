f07f974152fcf4320a639966b5815ede338717ec
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-19 21:33:04 +0300

    Zadacha23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,18 @@
+﻿
+void Zadacha23 ()
+{
+    Console.WriteLine("Введите целое число");
+    
+    int N = Convert.ToInt32(Console.ReadLine());
+    int i = 1;
+    double cube;
+    while (i <= N)
+    {
+        cube = Math.Pow(i,3);
+        Console.WriteLine($"{i} *{i} = {cube}");
+        i ++;
+
+    }
+}
+
+Zadacha23 ();
